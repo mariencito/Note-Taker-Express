6d55bbb1c40725a78462d35bf29a1b2a833c3a2e
marien <mariencastellanosjr@gmail.com>
2023-08-30 21:19:01 -0400

    saving work, need to link correctly

---
+++ routes/notes.js
@@ -0,0 +1,52 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+// DELETE Route for a specific tip
+notes.delete('/:tip_id', (req, res) => {
+  const tipId = req.params.tip_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.tip_id !== tipId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${tipId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text} = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    const parsedData = readAndAppend(newNote, './Develop/db/db.json');
+    res.json(parsedData);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+module.exports = notes;
